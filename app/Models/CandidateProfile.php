6e235e6f3975909be487a8e42c8f33d878f32d5d
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-02-01 20:33:26 +0100

    creer le model candidate_profile

--- app/Models/CandidateProfile.php
+++ app/Models/CandidateProfile.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class CandidateProfile extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'bio',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function educations()
+    {
+        return $this->hasMany(Education::class);
+    }
+
+    public function experiences()
+    {
+        return $this->hasMany(Experience::class);
+    }
+
+    public function skills()
+    {
+        return $this->belongsToMany(Skill::class);
+    }
 }
