50dd36f436e34cfdf90e9f2feefc5b0f3bf13c13
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-02-01 20:35:01 +0100

    creer le model expirience

--- app/Models/Experience.php
+++ app/Models/Experience.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Experience extends Model
 {
-    //
+    protected $fillable = [
+        'candidate_profile_id',
+        'position',
+        'company',
+        'start_date',
+        'end_date',
+    ];
+
+    public function candidateProfile()
+    {
+        return $this->belongsTo(CandidateProfile::class);
+    }
 }
