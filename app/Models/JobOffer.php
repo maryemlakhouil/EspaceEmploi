ac7bf15c4c39861ab09a4ce5ee0aae29913ad975
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-02-01 20:35:45 +0100

    creer le model joboffre

--- app/Models/JobOffer.php
+++ app/Models/JobOffer.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class JobOffer extends Model
 {
-    //
+    rotected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'contract_type',
+        'image',
+    ];
+
+    public function recruiter()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }
